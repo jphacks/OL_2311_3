a27913c76653e22d4269eb4f99315153d5c7d369
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-10-29 04:35:10 +0900

    fix: env var

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
 import { NextRequest, NextResponse } from "next/server"
 
-const BASIC_AUTH_USER = process.env["BASIC_AUTH_USER"]!
+const BASIC_AUTH_USERNAME = process.env["BASIC_AUTH_USERNAME"]!
 const BASIC_AUTH_PASSWORD = process.env["BASIC_AUTH_PASSWORD"]!
 
 /**
  * @see https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
  */
-// export const config = {
-//   matcher: ["/:path*", "/index/:path*"],
-// }
+export const config = {
+  matcher: ["/:path*", "/index/:path*"],
+}
 
 export function middleware(req: NextRequest) {
   if (process.env.NODE_ENV === "development") {
@@ -17,23 +17,22 @@ export function middleware(req: NextRequest) {
 
   const basicAuth = req.headers.get("Authorization")
 
-  if (basicAuth) {
-    const authValue = basicAuth.split(" ")[1]
-    // atob is deprecated but Buffer.from is not available in Next.js edge.
-    const [user, password] = atob(authValue ?? "").split(":")
-
-    if (user === BASIC_AUTH_USER && password === BASIC_AUTH_PASSWORD) {
-      return NextResponse.next()
-    }
-
-    return NextResponse.json(
-      { error: "Invalid credentials" },
-      { headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' }, status: 401 },
-    )
-  } else {
+  if (basicAuth == null) {
     return NextResponse.json(
       { error: "Please enter credentials" },
       { headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' }, status: 401 },
     )
   }
+
+  const authValue = basicAuth.split(" ")[1]
+  const [user, password] = atob(authValue ?? "").split(":")
+
+  if (user === BASIC_AUTH_USERNAME && password === BASIC_AUTH_PASSWORD) {
+    return NextResponse.next()
+  }
+
+  return NextResponse.json(
+    { error: "Invalid credentials" },
+    { headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' }, status: 401 },
+  )
 }
