9b36c050a83ee79f3264bc477c98f49ed94bac1a
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-11-18 09:00:56 +0900

    fix: firebase project name

--- src/lib/firebase/dev.ts
+++ src/lib/firebase/dev.ts
@@ -3,6 +3,6 @@ import { getFirestore } from "firebase/firestore"
 
 import { devConfig } from "./config"
 
-export const devApp = initializeApp(devConfig)
+export const devApp = initializeApp(devConfig, "dev")
 
 export const devDb = getFirestore(devApp)
